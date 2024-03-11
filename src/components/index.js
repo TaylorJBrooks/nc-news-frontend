d4874096aa69818ce729c82e51c7b72c8a251ccb
TaylorJBrooks <taylorjanebrooks@gmail.com>
2024-03-11 15:34:52 +0000

    single article page basic functionality created, with comments list

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,9 @@ import Header from "./Header";
 import Home from "./Home";
 import ArticlesList from "./ArticlesList";
 import ArticleCard from "./ArticleCard";
+import SingleArticle from "./SingleArticle";
+import CommentsList from "./CommentsList";
+import CommentCard from "./CommentCard";
+import Loading from "./Loading";
 
-export {Header, Home, ArticlesList, ArticleCard};
+export {Header, Home, ArticlesList, ArticleCard, SingleArticle, CommentsList, CommentCard, Loading};
