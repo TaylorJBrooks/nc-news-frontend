ece8e2b3819198fe2d9c18f8e2803667251e1638
TaylorJBrooks <taylorjanebrooks@gmail.com>
2024-03-12 11:52:45 +0000

    set up functionality for a user to log in

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,8 +4,12 @@ import { UserContext } from '../contexts/User'
 export default function UserCard({user}) {
     const { setLoggedInUser } = useContext(UserContext);
 
+    function handleUserCardClick(){
+        setLoggedInUser({...user, loggedIn: true})
+    }
+
   return (
-    <li className='user-card'>
+    <li className='user-card' onClick={handleUserCardClick}>
         <img src={user.avatar_url} className='user-card-section'/>
         <div className='user-card-section'>
             <h3>{user.name}</h3>
