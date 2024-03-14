68299a54b3b0c32783c38dcfa211d470eb47629b
TaylorJBrooks <taylorjanebrooks@gmail.com>
2024-03-14 16:43:57 +0000

    added pages to format of ArticlesList and CommentsList

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -5,21 +5,23 @@ import CommentCard from "./CommentCard";
 import AddComment from "./AddComment";
 import ErrorPage from "./ErrorPage";
 
-export default function CommentsList({ article_id }) {
+export default function CommentsList({ selectedArticle, setSelectedArticle }) {
   const [commentsList, setCommentsList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(5)
 
   useEffect(() => {
     setIsLoading(true);
-    getCommentsByArticleId(article_id).then(({ comments }) => {
+    const articleId = selectedArticle.article_id
+    getCommentsByArticleId({articleId, limit}).then(({ comments }) => {
       setCommentsList(comments);
       setIsLoading(false);
       setError(null);
     }).catch((err)=>{
       setError({err})
     });
-  }, []);
+  }, [limit]);
 
   if(error) {
     return <ErrorPage error={error.err.response}/>
@@ -30,12 +32,18 @@ export default function CommentsList({ article_id }) {
   ) : (
     <div>
         <h2>Comments:</h2>
-      <AddComment setCommentsList={setCommentsList} article_id={article_id}/>
+      <AddComment setCommentsList={setCommentsList} article_id={selectedArticle.article_id}/>
     <ul className="comments-list">
       {commentsList.map((comment) => {
         return <CommentCard key={comment.comment_id} comment={comment}/>;
       })}
     </ul>
+    <button
+    onClick={() => {
+      setLimit((currentLimit)=>currentLimit + 5)
+    }}
+    disabled={limit >= selectedArticle.comment_count}
+    >See More</button>
     </div>
   );
 }
